2a45bfcc606e0b92438a789bb580e5daaec9b650
jotaro-sama <36264038+jotaro-sama@users.noreply.github.com>
2019-11-07 15:23:15 +0100

    add first API test

---
+++ visualization.js
@@ -0,0 +1,16 @@
+function asyncGetRequest(requestURL, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", requestURL, true); 
+    xmlHttp.send(null);
+}
+
+const CV_IT_STATS_URL = "https://voice.mozilla.org/api/v1/it/clips/stats"
+
+asyncGetRequest(CV_IT_STATS_URL, function(response) {
+    console.log(response);
+});
